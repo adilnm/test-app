8cc7f44f865de32b5503ac1565bd2c3613235dd1
Adil Naimi <adilnm23@gmail.com>
2020-04-27 00:45:09 -0500

    Create a form to inpute notes that will be displayed to the homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      newNote:'',
+      notes:[]
+    }
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name]:e.target.value
+    })
+    
+  }
+
+  handleSubmit=(e)=>{
+    e.preventDefault()
+    this.setState({
+      notes:[...this.state.notes,this.state.newNote]
+    })
+
+  }
+
   render() {
     return (
       <div>
-        dddd
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="text" name="newNote"/>
+          <input type="submit"/>
+        </form>
+        <ul>
+          {this.state.notes.map((note,index)=><li key={index}>{note}</li>)}
+        </ul>
       </div>
     )
   }
